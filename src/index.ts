28901ad7c34733eadb8e9b41ca8471a2454e6f17
MiltonCandelero <miltoncandelero@gmail.com>
2020-09-03 22:58:43 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import { Application, Sprite } from 'pixi.js'
+
+const app = new Application({
+	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
+	resolution: window.devicePixelRatio || 1,
+	backgroundColor: 0x6495ed,
+	width: 640,
+	height: 480
+});
+
+const clampy: Sprite = Sprite.from("clampy.png");
+
+clampy.anchor.set(0.5);
+
+clampy.x = app.screen.width / 2;
+clampy.y = app.screen.height / 2;
+
+app.stage.addChild(clampy);
