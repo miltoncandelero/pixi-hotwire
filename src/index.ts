86c4d99c80bcd0e74a20faf94923e0549f76c01c
eCode <miltoncandelero@gmail.com>
2024-03-13 17:21:26 -0300

    refactor: top level await and pixi 8.0.2

--- src/index.ts
+++ src/index.ts
@@ -2,29 +2,27 @@ import { Application, Assets, Renderer, Sprite } from 'pixi.js'
 
 const app = new Application<Renderer<HTMLCanvasElement>>();
 
-// V8 change: App initialization is now async
-(async () => {
-    await app.init({
-		canvas: document.getElementById("pixi-canvas") as HTMLCanvasElement,
-		resolution: window.devicePixelRatio || 1,
-		autoDensity: true,
-		backgroundColor: 0x6495ed,
-		width: 640,
-		height: 480,
-		hello:true // Added so you can see what renderer are you using
-    });
-	
+// Top Level Await
 
-	// V8 change: You MUST load first. You can't use the `from` method without loading first.
-	await Assets.load("clampy.png");
+await app.init({
+	canvas: document.getElementById("pixi-canvas") as HTMLCanvasElement,
+	resolution: window.devicePixelRatio || 1,
+	autoDensity: true,
+	backgroundColor: 0x6495ed,
+	width: 640,
+	height: 480,
+	hello:true // Added so you can see what renderer are you using
+});
 
-	const clampy: Sprite = Sprite.from("clampy.png");
-	
-	clampy.anchor.set(0.5);
-	
-	clampy.x = app.screen.width / 2;
-	clampy.y = app.screen.height / 2;
-	
-	app.stage.addChild(clampy);
 
-})()
+// V8 change: You MUST load first. You can't use the `from` method without loading first.
+await Assets.load("clampy.png");
+
+const clampy: Sprite = Sprite.from("clampy.png");
+
+clampy.anchor.set(0.5);
+
+clampy.x = app.screen.width / 2;
+clampy.y = app.screen.height / 2;
+
+app.stage.addChild(clampy);
